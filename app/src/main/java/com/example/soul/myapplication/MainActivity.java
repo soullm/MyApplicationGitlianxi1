d6f656a2ef3e3be6a9ab63af02eb8c5d2238a3ab
soul <790166528@qq.com>
2018-05-24 10:46:56 +0800

    2018年5月24日10:46:49

--- app/src/main/java/com/example/soul/myapplication/MainActivity.java
+++ app/src/main/java/com/example/soul/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //sdfsdfdsfafsdafsafasfasfasfadsfdsafdsafasdf
+        //sdfsdfdsfafsdafsafasfasfasfadsfdsafdsafasdf尖峰时刻大房间卡的积分
     }
 }
